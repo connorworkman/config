30d9181f34dec3a00ef6bdbd6dbb4258c06d814f
alyptik <alyptik@protonmail.com>
2016-09-28 12:31:31 -1000

    Configuration backup on Wed Sep 28 12:31:31 HST 2016.

--- aliases.sh
+++ aliases.sh
@@ -182,7 +182,7 @@ alias ald='ld -I/lib64/ld-linux-x86-64.so.2 /usr/lib/crt1.o /usr/lib/crti.o -lc 
 
 tunssh() {
 	#sshuttle -r username@sshserver 0.0.0.0/0 -vv
-	sshuttle -r "${1?Erro: no host specified.}"  "${2:-0.0.0.0/0}" -vv
+	sshuttle -r "${1?Erro: no host specified.}"  "${2:-0.0.0.0/0}" -vv --dns
 }
 
 web_search_custom() {
