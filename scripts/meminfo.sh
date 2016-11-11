0ca256b842af2283b7b228d0c7bb36dbf615763d
alyptik <alyptik@protonmail.com>
2016-11-11 10:42:24 -1000

    Configuration backup on 2016-11-11 10:42 AM HST.

--- scripts/meminfo.sh
+++ scripts/meminfo.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 #RAM=`cat /proc/meminfo | grep "MemFree" | awk -F" " '{print $2}'`
-RAM=`free -h | grep "Mem:" | awk '{print $3 "/" $2}'`
-#RAM=`free -h | grep "Mem:" | awk '{print $3}'`
+#RAM=`free -h | grep "Mem:" | awk '{print $3 "/" $2}' | sed 's/\([GM]\)/\1iB/g'`
+RAM=`free -h --gibi | grep "Mem:" | awk '{print $3 "/" $2}'`
+
 #SWAP=`cat /proc/meminfo | grep "SwapFree" | awk -F" " '{print $2}'`
-SWAP=`free -h | grep "Swap:" | awk '{print $3 "/" $2}'`
-#SWAP=`free -h | grep "Swap:" | awk '{print $3}'`
-echo -n "RAM-${RAM} Swap-${SWAP}"
+#SWAP=`free -h | grep "Swap:" | awk '{print $3 "/" $2}' | sed 's/\([GM]\)/\1iB/g'`
+SWAP=`free -h --gibi | grep "Swap:" | awk '{print $3 "/" $2}'`
+
+printf '%s' "RAM-${RAM} Swap-${SWAP}"
